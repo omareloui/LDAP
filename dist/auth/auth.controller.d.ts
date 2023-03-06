e4c0f63eb42b8779747e94e9e8a43f0194a7d656
Omar Eloui <contact@omareloui.com>
2023-03-06 17:26:08 +0200

    chore(dist): remove committed 'dist'

--- dist/auth/auth.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AuthDto } from './auth.dto';
-import { AuthService } from './auth.service';
-export declare class AuthController {
-    private readonly authService;
-    constructor(authService: AuthService);
-    authenticate(body: AuthDto): Promise<{
-        authentacted: boolean;
-    }>;
-}
--- dist/config/index.d.ts
+++
@@ -1,5 +0,0 @@
-export default function (): {
-    LDAP_URL: string;
-    LDAP_BASE_DN: string;
-    LDAP_TIMEOUT: number;
-};
