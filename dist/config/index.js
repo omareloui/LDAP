813efdd009b05e04ed55f0bf6b0478b0bafb9381
Omar Eloui <contact@omareloui.com>
2023-03-06 17:22:55 +0200

    chore(config): remove config default

--- dist/config/index.js
+++ dist/config/index.js
@@ -1,11 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 function default_1() {
-    return {
-        LDAP_URL: process.env.LDAP_URL || 'ldap://AAIBank.Root.Local',
-        LDAP_BASE_DN: process.env.LDAP_BASE_DN || 'DC=AAIBank, DC=Root, DC=Local',
-        LDAP_TIMEOUT: parseInt(process.env.LDAP_TIMEOUT || '120000', 10),
-    };
+  return {
+    LDAP_URL: process.env.LDAP_URL,
+    LDAP_BASE_DN: process.env.LDAP_BASE_DN,
+    LDAP_TIMEOUT: parseInt(process.env.LDAP_TIMEOUT || '120000', 10),
+  };
 }
 exports.default = default_1;
 //# sourceMappingURL=index.js.map
