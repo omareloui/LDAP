1da5737c78de205ca2b29cdc45ab5336b3968ea8
Omar Eloui <contact@omareloui.com>
2023-03-06 17:24:08 +0200

    chore(ignore): ignore dist and fix config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 export default function () {
   return {
-    LDAP_URL: process.env.LDAP_URL || 'ldap://AAIBank.Root.Local',
-    LDAP_BASE_DN: process.env.LDAP_BASE_DN || 'DC=AAIBank, DC=Root, DC=Local',
+    LDAP_URL: process.env.LDAP_URL,
+    LDAP_BASE_DN: process.env.LDAP_BASE_DN,
     LDAP_TIMEOUT: parseInt(process.env.LDAP_TIMEOUT || '120000', 10),
   };
 }
